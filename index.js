d63f03e11d65ee47bdb41ac476eb331e6c38f532
dudi709 <dudi709@gmail.com>
2020-09-24 10:03:16 +0300

    Backup new files

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const express = require('express')
+const path = require("path");
+const express = require("express");
+
+const app = new express();
+
+const { engine } = require('express-edge');
 
 
 
-const app = new express()
 
+app.use(express.static("public"));
+
+app.use(engine);
+
+app.set('views', `${__dirname}/views`);
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+});
 
 app.listen(4000, () => {
     console.log("App listen on port 4000")
-})
+});
